69606a405a21813acdc0898f415c9c89e36daef1
Maksim Rabinovich <redfox@wisebits.com>
2021-09-09 17:32:40 +0300

    Fix bionic links, add crontab_example

--- test_check_console_bionic.py
+++ test_check_console_bionic.py
@@ -1,4 +1,5 @@
 import os
+import time
 
 import pytest
 
@@ -8,13 +9,15 @@ from pages.product_page import ProductPage
 from pages.login_page import LoginPage
 
 xbox = "https://bionic.com.cy/products/xbox-series-x-1tb-ssd-with-1-controller"
-ps5digital = "https://bionic.com.cy/products/sony-playstation-5-ps5-digital-edition"
+ps5digital = "https://bionic.com.cy/products/sony-playstation-5-ps5-digital-edition-18300387"
 ps5disc = "https://bionic.com.cy/products/sony-playstation-5-ps5-standard-edition"
+ps5ratchet = "https://bionic.com.cy/products/sony-playstation-5-ps5-standard-edition-825gb-ssd-with-sft-game-ratchet" \
+             "-clank-rift-apart "
 
 testProduct = "https://bionic.com.cy/products/bny-power-bank-2800mah"
 testProduct2 = "https://bionic.com.cy/products/xiaomi-mi-11-5g"
 
-consoles = [ps5digital, ps5disc, xbox]
+consoles = [ps5digital, ps5disc, ps5ratchet, xbox]
 login = "https://bionic.com.cy/login"
 
 
